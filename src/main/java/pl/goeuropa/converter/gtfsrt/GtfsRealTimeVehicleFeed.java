d3320e684dd4f0c539fc0e93f20e18f2c4a78fe9
Timur Shykhsefiyeu <nut.t@icloud.com>
2024-08-23 11:47:18 +0200

    use "number" key instead of "veh_title"

--- src/main/java/pl/goeuropa/converter/gtfsrt/GtfsRealTimeVehicleFeed.java
+++ src/main/java/pl/goeuropa/converter/gtfsrt/GtfsRealTimeVehicleFeed.java
@@ -35,9 +35,9 @@ public class GtfsRealTimeVehicleFeed {
         vehicles.forEach((vehicle) -> {
             GtfsRealtime.FeedEntity.Builder vehiclePositionEntity = GtfsRealtime.FeedEntity
                     .newBuilder()
-                    .setId(String.valueOf(vehicle.get("number") != null ?
-                            vehicle.get("number") :
-                            vehicle.get("vehicle_title")));
+                    .setId(String.valueOf(vehicle.get("unit_id") != null ?
+                            vehicle.get("unit_id") :
+                            vehicle.get("number")));
             try {
                 // the Vehicle data input
                 GtfsRealtime.VehiclePosition vehiclePosition = createVehiclePosition(vehicle);
@@ -56,7 +56,7 @@ public class GtfsRealTimeVehicleFeed {
         GtfsRealtime.VehiclePosition.Builder vehiclePosition = GtfsRealtime.VehiclePosition.newBuilder();
         // the Description information
         GtfsRealtime.VehicleDescriptor.Builder vehicleDescriptor = GtfsRealtime.VehicleDescriptor.newBuilder()
-                .setId(String.valueOf(vehicle.get("vehicle_title")));
+                .setId(String.valueOf(vehicle.get("number")));
         // the Position information
         GtfsRealtime.Position.Builder position =
                 GtfsRealtime.Position.newBuilder()
