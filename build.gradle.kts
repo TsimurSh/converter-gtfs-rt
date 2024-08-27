80e4661ecf5802ab24561313050afd305bbf37be
Timur Shykhsefiyeu <nut.t@icloud.com>
2024-08-27 12:45:41 +0200

    bump spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.9.22"
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.5"
 }
 
@@ -18,6 +18,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-validation")
+
     implementation("com.google.transit:gtfs-realtime-bindings:0.0.4")
 
     compileOnly("org.projectlombok:lombok")
