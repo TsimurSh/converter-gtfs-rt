b64541d2dbf17979c96dbf19a65c04fbf111ea9e
Timur Shykhsefiyeu <91431503+TsimurSh@users.noreply.github.com>
2025-11-15 20:36:07 +0100

    Update WebConfiguration.java

--- src/main/java/pl/goeuropa/tc_helper/configs/WebConfiguration.java
+++ src/main/java/pl/goeuropa/tc_helper/configs/WebConfiguration.java
@@ -1,6 +1,5 @@
-package org.transitclock.config;
+package pl.goeuropa.tc_helper.configs;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,15 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.jwt.issuer-uri", matchIfMissing = true, havingValue = "false")
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOriginPatterns("http://**", "https://**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*", "Authorization")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
