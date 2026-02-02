9073d4567107fcae1ebf5f48e31680a8a01ccac0
Timur S. <nut.t@icloud.com>
2026-02-02 13:59:59 +0100

    resolving issues - new sources

--- src/main/java/pl/goeuropa/converter/controller/VehicleUpdateController.java
+++ src/main/java/pl/goeuropa/converter/controller/VehicleUpdateController.java
@@ -1,8 +1,13 @@
 package pl.goeuropa.converter.controller;
 
 
+import com.google.transit.realtime.GtfsRealtime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +22,26 @@ public class VehicleUpdateController {
 
     private final VehicleUpdateService service;
 
-    @GetMapping("/vehiclePositions.text")
+    @GetMapping(value = "/vehiclePositions.text", produces = {MediaType.TEXT_PLAIN_VALUE})
     public String get() {
         var asText = service.getVehiclePositions();
         log.info("Get feed message include {} lines", asText.lines()
                 .count());
         return asText;
+    }
+
+    @GetMapping(value = "/vehiclePositions.pb", produces = {MediaType.APPLICATION_PROTOBUF_VALUE})
+    public ResponseEntity<Object> getGtfsRealtimeVehiclePositionsFeed() {
+        // Determine if output should be in human-readable format or in
+        // standard binary GTFS-realtime format.
+
+        GtfsRealtime.FeedMessage message = service.getPBVehiclePositions();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROTOBUF);
+        log.info("Get feed message include {} entities", message.getEntityList().size());
+        return ResponseEntity.ok()
+                .headers(headers)
+                .cacheControl(CacheControl.noCache())
+                .body(message);
     }
 }
--- src/main/java/pl/goeuropa/converter/service/ScheduledTasksService.java
+++ src/main/java/pl/goeuropa/converter/service/ScheduledTasksService.java
@@ -1,20 +1,24 @@
 package pl.goeuropa.converter.service;
 
 import com.google.transit.realtime.GtfsRealtime;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import pl.goeuropa.converter.client.ODWClient;
+import pl.goeuropa.converter.client.WrocClient;
 import pl.goeuropa.converter.dto.VehicleDto;
 import pl.goeuropa.converter.gtfsrt.GtfsRealTimeVehicleFeed;
 import pl.goeuropa.converter.repository.VehicleRepository;
 
 import java.io.FileOutputStream;
-import java.io.StringReader;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -22,27 +26,38 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class ScheduledTasksService {
 
-    private final ODWClient client;
+    private final WrocClient client;
+    private final VehicleRepository repository = VehicleRepository.getInstance();
 
     @Value("${api.out-path}")
     private String outPutPath;
+    @Value("${api.html-path}")
+    private String htmlPath;
 
-    private final VehicleRepository repository = VehicleRepository.getInstance();
 
     @Scheduled(fixedRateString = "${api.get-interval}",
             timeUnit = TimeUnit.SECONDS)
-    public void getAndParseData() {
+    public void getAndParseParams() {
 
         try {
-            var csvData = client.get();
+            var html = Jsoup.connect(htmlPath).get().outerHtml();
+            Document doc = Jsoup.parse(html);
 
-            CsvToBean<VehicleDto> csvToBean = new CsvToBeanBuilder<VehicleDto>(new StringReader(csvData))
-                    .withType(VehicleDto.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
+            List<String> busLines;
+            List<String> tramLines;
 
-            repository.setVehiclesList(csvToBean.parse());
-            log.debug("Get : {} entities.", repository.getVehiclesList().size());
+            Elements busElements = doc.select("div.bus-gps-lines.bus li.line");
+            busLines = busElements.stream().map(Element::text).toList();
+
+            Elements tramElements = doc.select("div.bus-gps-lines.tram li.line");
+            tramLines = tramElements.stream().map(Element::text).toList();
+
+            var vehiclesList = getVehiclesDtosFromRemote(busLines, tramLines)
+                    .stream()
+                    .filter(VehicleDto::validateLatLon).toList();
+            repository.setVehiclesList(vehiclesList);
+
+            log.debug("Get : {} entities.", vehiclesList.size());
         } catch (Exception ex) {
             log.error("Something get wrong while getting and parsing data", ex);
         }
@@ -61,5 +76,16 @@ public class ScheduledTasksService {
         } catch (Exception ex) {
             log.error(ex.getMessage());
         }
+    }
+
+    private Collection<VehicleDto> getVehiclesDtosFromRemote(List<String> busLines, List<String> tramLines) throws InterruptedException {
+        StringBuilder requestBody = new StringBuilder();
+        busLines.forEach(busLine -> requestBody.append("busList[bus][]=").append(busLine).append("&"));
+        var responseBys = client.getPositions(requestBody.toString());
+        Thread.sleep(2000);
+        StringBuilder tramRequestBody = new StringBuilder();
+        tramLines.forEach(tramLine -> tramRequestBody.append("busList[tram][]=").append(tramLine).append("&"));
+        var responseTram = client.getPositions(tramRequestBody.toString());
+        return CollectionUtils.union(responseBys, responseTram);
     }
 }
